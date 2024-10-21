56ae6837c931d2dd76d2b61d58c9cbdf9eb35b54
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-21 11:38:11 -0600

    Armar estructura header

--- header.php
+++ header.php
@@ -1,51 +1,193 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
-
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
-
+		<meta charset="<?php bloginfo("charset"); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+		<meta name="description" content="<?php bloginfo("description"); ?>">
+		<title><?php
+  wp_title("");
+  if (wp_title("", false)) {
+      echo " : ";
+  }
+  bloginfo("name");
+  ?></title>
+
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo(
+      "name"
+  ); ?>" href="<?php bloginfo("rss2_url"); ?>" />
+
+		<link
+            rel="icon"
+            type="image/png"
+            href="<?php echo esc_url(
+                get_template_directory_uri()
+            ); ?>/favicon-48x48.png"
+            sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/favicon.svg" />
+        <link rel="shortcut icon" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/favicon.ico" />
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="<?php echo esc_url(
+                get_template_directory_uri()
+            ); ?>/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Logos" />
+        <link rel="manifest" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/site.webmanifest" />
+
+        <link rel="stylesheet" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/assets/css/styles.css" />
 
 		<?php wp_head(); ?>
-		<script>
-		// conditionizr.com
-		// configure environment tests
-		conditionizr.config({
-			assets: '<?php echo esc_url( get_template_directory_uri() ); ?>',
-			tests: {}
-		});
-		</script>
+
 
 	</head>
-	<body <?php body_class(); ?>>
+	<body>
+        <div id="backdrop"></div>
+        <div class="menu">
+            <a id="cerrar-menu" href="javascript:void(0)">
+                <i class="fas fa-times"></i>
+            </a>
+            <div class="menu-contenido">
+                <a class="anchor" id="btn-logo" href="<?php echo esc_url(
+                    home_url()
+                ); ?>">
+                    <img
+                        class="logo img-fluid"
+                        src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/logo@2x.png"
+                        alt=""
+                    />
+                </a>
+                <nav>
+                    <ul id="navmenu" class="list-unstyled mb-0">
+                        <li>
+                            <a class="anchor" id="btn-nav-1" href="<?php if (
+                                !is_home()
+                            ):
+                                echo esc_url(home_url());
+                            endif; ?>#nosotros"
+                                >Nosotros</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-2" href="<?php if (
+                                !is_home()
+                            ):
+                                echo esc_url(home_url());
+                            endif; ?>#servicios"
+                                >Servicios</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-3" href="<?php if (
+                                !is_home()
+                            ):
+                                echo esc_url(home_url());
+                            endif; ?>#testimonios"
+                                >Testimonios</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-4" href="<?php if (
+                                !is_home()
+                            ):
+                                echo esc_url(home_url());
+                            endif; ?>#equipo"
+                                >Equipo</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-5" href="<?php if (
+                                !is_home()
+                            ):
+                                echo esc_url(home_url());
+                            endif; ?>#blog"
+                                >Blog</a
+                            >
+                        </li>
+                    </ul>
+                </nav>
+                <a
+                    href="javascript:void(0)"
+                    class="btn btn-secondary"
+                    id="btn-contacto"
+                    data-bs-toggle="modal"
+                    data-bs-target="#modal-contacto"
+                    >Contáctanos</a
+                >
+                <div id="social">
+                    <ul class="social list-inline">
+                        <li class="list-inline-item">
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a
+                                href="https://www.instagram.com/"
+                                target="_blank"
+                            >
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://x.com/" target="_blank">
+                                <i class="fa-brands fa-x-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://linkedin.com/" target="_blank">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="contacto-menu">
+                    <ul class="list-unstyled">
+                        <li>
+                            <i class="far fa-envelope"></i>
+                            <a href="mailto:">mail@logosregenerative.com</a>
+                        </li>
+                        <li>
+                            <i class="fas fa-phone"></i>
+                            <a href="tel:+526141234567">(614) 123 4567</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
-		<!-- wrapper -->
-		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo esc_url( home_url() ); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
+        <header id="navbar">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-4 my-auto">
+                        <a href="<?php echo esc_url(home_url()); ?>">
+                            <img
+                                src="<?php echo esc_url(
+                                    get_template_directory_uri()
+                                ); ?>/assets/images/logo@2x.png"
+                                alt=""
+                                class="logo img-fluid"
+                                id="logo-navbar"
+                            />
+                        </a>
+                    </div>
+                    <div class="col-8 my-auto text-end">
+                        <a id="mburger" href="javascript:void(0)">
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>

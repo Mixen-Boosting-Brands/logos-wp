30809e126e8bde2bacc69df1259c2a86bc23cf2f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-21 13:39:52 -0600

    Armar estructura single

--- single.php
+++ single.php
@@ -1,71 +1,59 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-	<!-- section -->
-	<section>
-
-	<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
-
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post. ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php esc_html_e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?></span>
-			<!-- /post details -->
-
-			<?php the_content(); // Dynamic Content. ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>' ); // Separated by commas with a line break at the end. ?>
-
-			<p><?php esc_html_e( 'Categorised in: ', 'html5blank' ); the_category( ', ' ); // Separated by commas. ?></p>
-
-			<p><?php esc_html_e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else : ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section id="interna" class="light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <?php the_post_thumbnail("full", [
+                    "class" => "rounded img-fluid mb-4",
+                ]); ?>
+                <?php if (is_single()) {
+                    $categories = get_the_category();
+                    if (!empty($categories)) {
+                        echo '<ul class="post-categories list-inline">';
+                        foreach ($categories as $category) {
+                            echo '<li class="list-inline-item"><a href="' .
+                                esc_url(get_category_link($category->term_id)) .
+                                '">' .
+                                esc_html($category->name) .
+                                "</a></li>";
+                        }
+                        echo "</ul>";
+                    }
+                } ?>
+                <h1><?php the_title(); ?></h1>
+                <?php if (have_posts()):
+                    while (have_posts()):
+                        the_post(); ?>
+         			<!-- article -->
+         			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            				<?php the_content(); ?>
+            				<?php comments_template("", true);
+                        // Remove if you don't want comments.
+                        ?>
+            				<br class="clear">
+            				<?php edit_post_link(); ?>
+         			</article>
+         			<!-- /article -->
+          		<?php
+                    endwhile; ?>
+          		<?php
+                else:
+                     ?>
+         			<!-- article -->
+         			<article>
+            				<h2><?php esc_html_e(
+                    "Lo sentimos, no hay nada que mostrar aquí.",
+                    "html5blank"
+                ); ?></h2>
+         			</article>
+         			<!-- /article -->
+          		<?php
+                endif; ?>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>

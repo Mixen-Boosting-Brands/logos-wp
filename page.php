a6275ef51bb708e2e728bd2e8c5ad223bbb4095b
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-21 13:01:19 -0600

    Armar estructura page

--- page.php
+++ page.php
@@ -1,45 +1,57 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<h1><?php the_title(); ?></h1>
-
-		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments. ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+<section id="interna">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <?php the_post_thumbnail("large", [
+                    "class" => "rounded img-fluid",
+                ]); ?>
+                <?php if (is_single()) {
+                    $categories = get_the_category();
+                    if (!empty($categories)) {
+                        echo '<ul class="post-categories list-inline">';
+                        foreach ($categories as $category) {
+                            echo '<li class="list-inline-item"><a href="' .
+                                esc_url(get_category_link($category->term_id)) .
+                                '">' .
+                                esc_html($category->name) .
+                                "</a></li>";
+                        }
+                        echo "</ul>";
+                    }
+                } ?>
+                <h1><?php the_title(); ?></h1>
+                <?php if (have_posts()):
+                    while (have_posts()):
+                        the_post(); ?>
+         			<!-- article -->
+         			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            				<?php the_content(); ?>
+            				<?php comments_template("", true);
+                        // Remove if you don't want comments.
+                        ?>
+            				<br class="clear">
+            				<?php edit_post_link(); ?>
+         			</article>
+         			<!-- /article -->
+          		<?php
+                    endwhile; ?>
+          		<?php
+                else:
+                     ?>
+         			<!-- article -->
+         			<article>
+            				<h2><?php esc_html_e(
+                    "Lo sentimos, no hay nada que mostrar aquí.",
+                    "html5blank"
+                ); ?></h2>
+         			</article>
+         			<!-- /article -->
+          		<?php
+                endif; ?>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</section>

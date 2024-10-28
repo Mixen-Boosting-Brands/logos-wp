ce49c6077c105624a4e992a8dbd09a789f7b940b
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-28 17:07:09 -0600

    Agregar breadcrumbs

--- single.php
+++ single.php
@@ -6,9 +6,11 @@
             <div class="col-lg-9 mb-4 mb-lg-0" data-aos="fade-up"
             data-aos-duration="1000"
             data-aos-delay="100">
-                <?php if (function_exists("aioseo_breadcrumbs")) {
-                    aioseo_breadcrumbs();
-                } ?>
+                <div class="mb-2">
+                    <?php if (function_exists("aioseo_breadcrumbs")) {
+                        aioseo_breadcrumbs();
+                    } ?>
+                </div>
                 <?php the_post_thumbnail("full", [
                     "class" => "rounded img-fluid mb-4",
                 ]); ?>
